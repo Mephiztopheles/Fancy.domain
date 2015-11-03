b72dfb966e5e483e0fee6274d0827e6fc87f2ce6
Mephiztopheles <ma@pro-bit.de>
2015-11-03 15:00:56 +0100

    nullable

--- fancydomain.js
+++ fancydomain.js
@@ -1,9 +1,9 @@
-(function( Fancy ) {
+(function ( Fancy ) {
     "use strict";
     extend( Function.prototype, {
         wrap: function wrap( wrapper ) {
             var fn = this;
-            return function() {
+            return function () {
                 return wrapper.apply( this, [ fn.bind( this ) ].concat( Array.prototype.slice.call( arguments ) ) );
             }
         }
@@ -13,21 +13,28 @@
         return Array.prototype.slice.call( a );
     }
 
+    /**
+     * @return {string}
+     */
+    function FancyDomainError( type, msg ) {
+        return "Error[" + type + "]: " + msg;
+    }
+
     function addMethods( functions, fn ) {
         var $super = fn.parent && fn.parent.prototype;
-        each( functions, function( value, property ) {
-            if( $super && Fancy.getType( value ) === "function" ) {
+        each( functions, function ( value, property ) {
+            if ( $super && Fancy.getType( value ) === "function" ) {
                 var method = value;
-                value      = (function( m ) {
-                    return function() { return $super[ m ].apply( this, arguments ); };
+                value      = (function ( m ) {
+                    return function () { return $super[ m ].apply( this, arguments ); };
                 })( property ).wrap( method );
 
-                value.valueOf = (function( m ) {
-                    return function() { return m.valueOf.call( m ); };
+                value.valueOf = (function ( m ) {
+                    return function () { return m.valueOf.call( m ); };
                 })( method );
 
-                value.toString = (function( m ) {
-                    return function() { return m.toString.call( m ); };
+                value.toString = (function ( m ) {
+                    return function () { return m.toString.call( m ); };
                 })( method );
             }
             fn.prototype[ property ] = value;
@@ -37,19 +44,24 @@
     }
 
     function each( obj, callback ) {
-        if( Fancy.getType( obj ) === "array" ) {
-            if( Array.prototype.forEach ) {
+        var i;
+        if ( Fancy.getType( obj ) === "array" ) {
+            if ( Array.prototype.forEach ) {
                 obj.forEach( callback );
             } else {
-                for( var i in obj ) {
-                    if( i.toString().match( /^\d*$/ ) ) {
-                        callback( obj[ i ], i );
+                for ( i in obj ) {
+                    if ( obj.hasOwnProperty( i ) ) {
+                        if ( i.toString().match( /^\d*$/ ) ) {
+                            callback( obj[ i ], i );
+                        }
                     }
                 }
             }
-        } else if( Fancy.getType( obj ) === "object" ) {
-            for( var i in obj ) {
-                callback( obj[ i ], i );
+        } else if ( Fancy.getType( obj ) === "object" ) {
+            for ( i in obj ) {
+                if ( obj.hasOwnProperty( i ) ) {
+                    callback( obj[ i ], i );
+                }
             }
         }
     }
@@ -57,9 +69,11 @@
     function extend( a ) {
         var args = argumentToArray( arguments );
         args.shift();
-        each( args, function( it ) {
-            for( var i in it ) {
-                a[ i ] = it[ i ];
+        each( args, function ( it ) {
+            for ( var i in it ) {
+                if ( it.hasOwnProperty( i ) ) {
+                    a[ i ] = it[ i ];
+                }
             }
         } );
         return a;
@@ -69,71 +83,71 @@
 
     function Domain( name, data, options, functions ) {
         var parent = null;
-        if( Fancy.getType( name ) === "function" ) {
+        if ( Fancy.getType( name ) === "function" ) {
             parent = name;
         } else {
             functions = options;
             options   = data;
             data      = name;
             name      = null;
-            if( !(Object.keys( data )[ 0 ] in options) ) {
+            if ( !(data && Object.keys( data )[ 0 ] in options) ) {
                 functions = options;
                 options   = null;
             }
         }
-        Superclass.parent                = parent;
-        Superclass.subclasses            = [];
-        Superclass.$properties           = {};
-        Superclass.$constraints          = {};
+        Superclass.parent       = parent;
+        Superclass.subclasses   = [];
+        Superclass.$properties  = {};
+        Superclass.$constraints = {};
 
-        if( options ) {
-            each( options, function( it, i ) {
+        if ( options ) {
+            each( options, function ( it, i ) {
                 Superclass.$constraints[ i ] = it;
             } );
 
         }
-        if( data ) {
-            each( data, function( it, i ) {
+        if ( data ) {
+            each( data, function ( it, i ) {
                 var cons = Superclass.$constraints[ i ] || {};
-                if( Fancy.getType( it ) === "string" ) {
-                    switch( it.toLowerCase() ) {
+                if ( Fancy.getType( it ) === "string" ) {
+                    switch ( it.toLowerCase() ) {
                         case "number":
                         case "integer":
-                            if( Fancy.getType( cons.defaultValue ) === "number" ) {
+                            if ( Fancy.getType( cons.defaultValue ) === "number" ) {
                                 Superclass.$properties[ i ] = cons.defaultValue;
                             } else {
-                                Superclass.$properties[ i ] = 0;
+                                Superclass.$properties[ i ] = null;
                             }
                             break;
                         case "double":
                         case "float":
-                            if( Fancy.getType( cons.defaultValue ) === "number" ) {
+                            if ( Fancy.getType( cons.defaultValue ) === "number" ) {
                                 Superclass.$properties[ i ] = cons.defaultValue;
                             } else {
-                                Superclass.$properties[ i ] = 0.0;
+                                Superclass.$properties[ i ] = null;
                             }
                             break;
                         case "array":
                         case "list":
-                            if( Fancy.getType( cons.defaultValue ) === "array" ) {
+                            if ( Fancy.getType( cons.defaultValue ) === "array" ) {
                                 Superclass.$properties[ i ] = cons.defaultValue;
                             } else {
-                                Superclass.$properties[ i ] = 0.0;
+                                Superclass.$properties[ i ] = null;
                             }
                             break;
                         case "object":
                         case "map":
-                            if( Fancy.getType( cons.defaultValue ) === "object" ) {
-                                Superclass.$properties[ i ] = cons.defaultValue;
-                            } else {
-                                Superclass.$properties[ i ] = 0.0;
-                            }
-                            break;
-                        default:
-                            if( cons.defaultValue !== undefined ) {
+                            if ( Fancy.getType( cons.defaultValue ) === "object" ) {
                                 Superclass.$properties[ i ] = cons.defaultValue;
                             } else {
                                 Superclass.$properties[ i ] = null;
+                            }
+                            break;
+                        default:
+                            if ( cons.defaultValue === undefined ) {
+                                Superclass.$properties[ i ] = null;
+                            } else {
+                                Superclass.$properties[ i ] = cons.defaultValue;
                             }
                             break;
                     }
@@ -144,7 +158,7 @@
         } else {
             console.warn( "No properties?" );
         }
-        if( parent ) {
+        if ( parent ) {
             Subclass.prototype   = parent.prototype;
             Superclass.prototype = new Subclass;
             parent.subclasses.push( Superclass );
@@ -152,10 +166,19 @@
             extend( Superclass.$constraints, parent.$constraints );
         }
         function init( object ) {
-            if( object ) {
-                extend( this, Superclass.$properties, object );
-            }
+            extend( this, Superclass.$properties, object );
+            this.checkConstraints();
         }
+
+        Superclass.prototype.checkConstraints = function checkConstraints() {
+            var SELF = this;
+            each( Superclass.$constraints, function throwError( it, i ) {
+                if ( it.nullable === false && SELF[ i ] === null ) {
+                    throw FancyDomainError( "nullable", "You can not set \"" + i + "\" as null" );
+                }
+            } );
+            return SELF;
+        };
 
         function Superclass( object ) {
             init.call( this, object );
@@ -163,7 +186,7 @@
         }
 
         addMethods( functions, Superclass );
-        Superclass.prototype.constructor = Superclass;
+        Superclass.prototype.constructor      = Superclass;
         return Superclass;
     }
 
